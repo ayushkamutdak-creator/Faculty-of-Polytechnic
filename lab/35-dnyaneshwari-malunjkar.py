d2425c5c99a9a32903f19b32870807fa55144b4c
dnyaneshwarimalunjkar4-netizen <dnyaneshwarimalunjkar4@gmail.com>
2026-06-02 11:51:20 +0530

    Implement divisibility check for numbers a, b, c
    Added a loop to check divisibility by 3 for given numbers.

--- lab/35-dnyaneshwari-malunjkar.py
+++ lab/35-dnyaneshwari-malunjkar.py
@@ -4,3 +4,13 @@ while n<=10:
     t+=n
     n+=1
 print (t) 
+#problem
+a=4
+b=9
+c=19
+for num in [a, b, c]:
+    if num % 3 == 0:
+    print(num, "is divisble by 3") 
+else:
+    print(num, "is not divisble by 3") 
+
