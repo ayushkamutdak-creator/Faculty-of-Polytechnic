587607f9c5ddff4bc802a4e90eab464f770b980e
rameshpunde958-sketch <rameshpunde958@gmail.com>
2026-06-05 11:36:58 +0530

    Add solution1.py to count consonants

---
+++ students/bhakti-punde/solution1.py
@@ -0,0 +1,6 @@
+set={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+count=0
+for x in set:
+    if x not in ['a','e','i','o','u']:
+        count=count+1
+print (count)
