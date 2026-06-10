fd05ad1b18dae289f4aa2d60e46f4594fc5b5ae4
piyushkaaher1411-web <piyushkaaher1411@gmail.com>
2026-06-10 11:15:09 +0530

    Update solution5.py

--- students/piyushka-aher/solution5.py
+++ students/piyushka-aher/solution5.py
@@ -1,1 +1,8 @@
-
+#problem 1
+print("Start small. Ship something.")
+def rectangle(l,b):
+    area=l*b
+    return area
+l=float(input("enter length="))
+b=float(input("enter breath="))
+print("Area of rectangle ", rectangle (l,b))
